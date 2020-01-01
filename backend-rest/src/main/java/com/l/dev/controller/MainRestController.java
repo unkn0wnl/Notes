a290fadfc702c9e328d6f27620ace68c6b99b78d
L <ap.qwert@yandex.ru>
2020-01-01 23:03:09 +0300

    cross origin annotation for RestController

--- backend-rest/src/main/java/com/l/dev/controller/MainRestController.java
+++ backend-rest/src/main/java/com/l/dev/controller/MainRestController.java
@@ -5,14 +5,11 @@ import com.l.dev.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class MainRestController {
