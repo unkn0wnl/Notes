5c4a9adf92b9bfd64f833ff521f08b5bfb72adbf
L <ap.qwert@yandex.ru>
2020-01-20 16:54:26 +0300

    refactoring methods in UserService

--- backend-core/src/main/java/com/l/dev/service/UserService.java
+++ backend-core/src/main/java/com/l/dev/service/UserService.java
@@ -1,13 +1,10 @@
 package com.l.dev.service;
 
-import com.l.dev.entity.model.User;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public interface UserService {
-    void deleteUserById(Long id);
 
-    List<User> findAll();
+    void registerNewUser(String name, String username, String email, String hashedPassword);
+
 }
