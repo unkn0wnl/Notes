c3f60ae9dbe4f4da2d9306af9110289d7e9a99d7
L <ap.qwert@yandex.ru>
2020-01-20 16:50:23 +0300

    added User auditing

---
+++ backend-core/src/main/java/com/l/dev/entity/model/DateAudit.java
@@ -0,0 +1,46 @@
+package com.l.dev.entity.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+import java.time.Instant;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(
+        value = {"createAt", "updateAt"},
+        allowGetters = true
+)
+public abstract class DateAudit implements Serializable {
+
+    @CreatedDate
+    @Column(name = "update_at", nullable = false, updatable = false)
+    private Instant updateAt;
+
+    @LastModifiedDate
+    @Column(name = "create_at", nullable = false)
+    private Instant createAt;
+
+    public Instant getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Instant updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    public Instant getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Instant createAt) {
+        this.createAt = createAt;
+    }
+
+}
