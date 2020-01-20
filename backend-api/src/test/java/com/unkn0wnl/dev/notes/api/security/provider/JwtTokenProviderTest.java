4bf4cc2224284594a8b55f9592ceab23ed05ea6b
L <ap.qwert@yandex.ru>
2020-01-20 20:48:50 +0300

    added Mockito library

--- backend-api/src/test/java/com/unkn0wnl/dev/notes/api/security/provider/JwtTokenProviderTest.java
+++ backend-api/src/test/java/com/unkn0wnl/dev/notes/api/security/provider/JwtTokenProviderTest.java
@@ -1,24 +1,24 @@
-package com.unkn0wnl.dev.notes.core.repository;
+package com.unkn0wnl.dev.notes.api.security.provider;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.internal.util.MockUtil;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(MockitoExtension.class)
 @RunWith(JUnitPlatform.class)
-public class UserRepositoryTest {
+class JwtTokenProviderTest {
+
+    @Mock
+    JwtTokenProvider jwtTokenProvider;
 
     @Test
-    @ParameterizedTest
-    @ValueSource(strings = {"someString"})
-    public void someTestMethod() {
-        assertTrue(true);
+    void validateToken() {
+        assertTrue(MockUtil.isMock(jwtTokenProvider));
     }
-
 }
