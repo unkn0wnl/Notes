0bb791b653b3e57ff633479f5e6e71f9d07a0b83
L <ap.qwert@yandex.ru>
2020-01-23 16:31:55 +0300

    initialization code for web tests was placed in WebIntegrationTestConfigurer old tests was refactored

--- backend-api/src/test/java/com/unkn0wnl/dev/notes/api/controller/AuthenticationControllerTest.java
+++ backend-api/src/test/java/com/unkn0wnl/dev/notes/api/controller/AuthenticationControllerTest.java
@@ -1,55 +1,27 @@
 package com.unkn0wnl.dev.notes.api.controller;
 
 import com.unkn0wnl.dev.notes.api.payload.request.SingUpRequest;
-import org.apache.logging.log4j.Logger;
+import config.WebIntegrationTestConfigurer;
 import org.codehaus.jackson.map.ObjectMapper;
-import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.context.WebApplicationContext;
 
-import static org.apache.logging.log4j.LogManager.getLogger;
-import static org.mockito.MockitoAnnotations.initMocks;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringRunner.class)
 @Transactional
 @WebAppConfiguration
 @ContextConfiguration("classpath:config/applicationContext-testConfig.xml")
-public class AuthenticationControllerTest {
+public class AuthenticationControllerTest extends WebIntegrationTestConfigurer {
 
-    private static final Logger LOGGER;
-
-    static {
-        LOGGER = getLogger(AuthenticationControllerTest.class);
-    }
-
-    @Autowired
-    WebApplicationContext webApplicationContext;
-
-    MockMvc mockMvc;
-
-    @Before
-    public void init() {
-        initMocks(this);
-        this.mockMvc = webAppContextSetup(this.webApplicationContext)
-                .alwaysDo(print())
-                .apply(springSecurity())
-                .build();
-    }
-
-    @org.junit.Test
+    @Test
     public void authenticateUser() throws Exception {
 
     }
---
+++ backend-api/src/test/java/config/WebIntegrationTestConfigurer.java
@@ -0,0 +1,40 @@
+package config;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.apache.logging.log4j.LogManager.getLogger;
+import static org.mockito.MockitoAnnotations.initMocks;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+
+public abstract class WebIntegrationTestConfigurer {
+
+    protected final Logger logger;
+    protected MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    {
+        Configurator.setRootLevel(Level.DEBUG);
+        logger = getLogger(getClass());
+    }
+
+    @Before
+    public void init() {
+        initMocks(this);
+        this.mockMvc = webAppContextSetup(this.webApplicationContext)
+                .alwaysDo(print())
+                .apply(springSecurity())
+                .build();
+    }
+
+}
