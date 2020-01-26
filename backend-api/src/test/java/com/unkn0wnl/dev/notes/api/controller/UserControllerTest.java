d8dd94fb38311e12379367df2cbe1506ccaf518f
L <ap.qwert@yandex.ru>
2020-01-26 20:34:53 +0300

    new test case

--- backend-api/src/test/java/com/unkn0wnl/dev/notes/api/controller/UserControllerTest.java
+++ backend-api/src/test/java/com/unkn0wnl/dev/notes/api/controller/UserControllerTest.java
@@ -2,6 +2,7 @@ package com.unkn0wnl.dev.notes.api.controller;
 
 
 import config.WebIntegrationTestConfigurer;
+import org.apache.logging.log4j.Level;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.http.MediaType;
@@ -13,13 +14,19 @@ import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @Transactional
 @WebAppConfiguration
 @ContextConfiguration("classpath:config/applicationContext-testConfig.xml")
 public class UserControllerTest extends WebIntegrationTestConfigurer {
+
+    public static final String IS_AVAILABLE = "/api/user/isAvailable";
+
+    static {
+        loggerLevel = Level.INFO;
+    }
 
     @Test
     @WithMockUser(roles = "USER")
@@ -29,6 +36,19 @@ public class UserControllerTest extends WebIntegrationTestConfigurer {
                         .contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andDo(print())
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    public void checkUserAvailability_TypeUsernameEmptyUsername_Return() throws Exception {
+        mockMvc.perform(
+                get(IS_AVAILABLE)
+                        .param("type", "username")
+                        .param("value", "")
+        )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.isAvailable").value("true"));
     }
 
 }
