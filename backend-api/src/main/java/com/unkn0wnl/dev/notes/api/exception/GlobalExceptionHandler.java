df9fc11ab295d3279c725d1186117f0a417e59df
L <ap.qwert@yandex.ru>
2020-01-27 19:30:58 +0300

    added logging exception

--- backend-api/src/main/java/com/unkn0wnl/dev/notes/api/exception/GlobalExceptionHandler.java
+++ backend-api/src/main/java/com/unkn0wnl/dev/notes/api/exception/GlobalExceptionHandler.java
@@ -1,17 +1,30 @@
 package com.unkn0wnl.dev.notes.api.exception;
 
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+
+import static org.apache.logging.log4j.LogManager.getLogger;
+
 @ControllerAdvice
 @RestController
 public class GlobalExceptionHandler {
 
+    private static final Logger LOGGER;
+
+    static {
+        LOGGER = getLogger(GlobalExceptionHandler.class);
+    }
+
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity<JsonResponse> handleException(Exception e) {
+        LOGGER.error(e);
+        LOGGER.error(Arrays.toString(e.getStackTrace()));
         return new ResponseEntity<JsonResponse>(new JsonResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
