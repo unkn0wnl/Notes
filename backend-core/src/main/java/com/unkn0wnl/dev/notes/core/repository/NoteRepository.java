008d96f7245c2594adaa43cac9f1095aa34a53f0
L <ap.qwert@yandex.ru>
2020-02-04 23:14:30 +0300

    minor improvements in repositories

--- backend-core/src/main/java/com/unkn0wnl/dev/notes/core/repository/NoteRepository.java
+++ backend-core/src/main/java/com/unkn0wnl/dev/notes/core/repository/NoteRepository.java
@@ -2,7 +2,9 @@ package com.unkn0wnl.dev.notes.core.repository;
 
 import com.unkn0wnl.dev.notes.core.entity.model.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NoteRepository extends JpaRepository<Note, Long> {
 
 }
